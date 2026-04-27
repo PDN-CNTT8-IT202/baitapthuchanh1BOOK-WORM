a6d8680c9d782633972e653840656be8b55d9a47
TLINHCUTE <phandinhnghi27062356@gmail.com>
2026-04-27 10:49:33 +0700

    baitapthuchanh2

--- baitapthuchanhBOOK-WORM.sql
+++ baitapthuchanhBOOK-WORM.sql
@@ -33,14 +33,29 @@ CREATE TABLE customers (
 
 INSERT INTO authors (full_name, birth_year, nationality)
 VALUES ('Nguyen Nhat Anh', 1955, 'Vietnam'),
+('Agatha Christie', 1890, 'UK'),
 ('J.K. Rowling', 1965, 'UK');
 
 INSERT INTO books (book_name, category, author_id, price, publish_year)
 VALUES('Cho toi xin mot ve di tuoi tho', 'Novel', 1, 50000, 2008),
-('Harry Potter', 'Fantasy', 2, 120000, 1997);
+('Mat biec', 'Van hoc', 1, 60000, 2010),
+('Toi thay hoa vang tren co xanh', 'Van hoc', 1, 55000, 2011),
+('Murder on the Orient Express', 'Trinh tham', 2, 80000, 1934),
+('And Then There Were None', 'Trinh tham', 2, 75000, 1939),
+('How to Win Friends and Influence People', 'Ky nang', 3, 90000, 1936),
+('Stop Worrying and Start Living', 'Ky nang', 3, 85000, 1948),
+('Tu duy nhanh va cham', 'Ky nang', 3, 95000, 2011);
 
 INSERT INTO customers (full_name, email, phone)
 VALUES('Nguyen Van A', 'a@gmail.com', '0123456789'),
+('Test Trung Email', 'a@gmail.com', '0999999999'),
 ('Tran Thi B', 'b@gmail.com', '0987654321');
+
+-- Giai thich:
+-- Truong email trong customer duoc thiet lap rang buoc unipue. Vi vay, khi co gang chen 1 ban ghi cs email đa ton tai ('a@gmail.com').
+-- Thi he quan tri CSDL se tu choi va bao loi Duplicate entry.
+-- Dieu nay dam bao du lieu ko bi trug lap va giu tinh toan ven.
+
+
 
 
